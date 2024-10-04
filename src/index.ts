ddb5316910e26c48f71cf2ee89c566daf6ebeede
Jesus Lopez <jesussflr@gmail.com>
2024-10-04 13:49:44 -0400

    first commit

---
+++ src/index.ts
@@ -0,0 +1,16 @@
+import { bootstrap, JobQueueService, runMigrations } from "@vendure/core";
+import { config } from "./vendure-config";
+import { populateOnFirstRun } from "./populate";
+
+populateOnFirstRun(config)
+  .then(() => runMigrations(config))
+  .then(() => bootstrap(config))
+  .then((app) => {
+    // For "lite" deployments with limited resources, we can run the job queue
+    if (process.env.RUN_JOB_QUEUE_FROM_SERVER?.toLowerCase() === "true") {
+      return app.get(JobQueueService).start();
+    }
+  })
+  .catch((err) => {
+    console.log(err);
+  });
