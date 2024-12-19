b3fe009c285e9cbcf687b5ba774c1f488ef0a60f
Jesus Lopez <jesuslopez@globuco.com>
2024-12-18 22:39:06 -0400

    update lotes list

--- src/plugins/lotes-plugin/ui/routes.ts
+++ src/plugins/lotes-plugin/ui/routes.ts
@@ -1,5 +1,5 @@
-import { registerReactRouteComponent } from "@vendure/admin-ui/react";
-import { LotesPage } from "./components/lotes-page/lotes-page";
+// import { registerReactRouteComponent } from "@vendure/admin-ui/react";
+// import { LotesPage } from "./components/lotes-page/lotes-page";
 import { LoteListComponent } from "./components/lotes-list/lotes-list.component";
 import { registerRouteComponent } from "@vendure/admin-ui/core";
 
