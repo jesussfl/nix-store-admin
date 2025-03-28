30c0de0da755a9c16c9c03cbb929f0d5b31be028
Jesus Lopez <jesuslopez@globuco.com>
2025-03-27 20:40:22 -0400

    chore: enhance admin UI configuration with improved output paths and translation support

--- src/vendure-config.ts
+++ src/vendure-config.ts
@@ -199,6 +199,25 @@ function compileAdminUi() {
     };
   }
   return {
-    path: path.join(__dirname, "../admin-ui"),
+    ...compileUiExtensions({
+      outputPath: IS_DEV ? path.join(__dirname, "../admin-ui") : path.join(__dirname, "../dist/admin-ui"),
+      devMode: IS_DEV ? true : false,
+      //   ngCompilerPath: path.join(__dirname, "./node_modules/.bin/ng"),
+      extensions: [
+        LotesPlugin.ui,
+        setBranding({
+          // The small logo appears in the top left of the screen
+          smallLogoPath: path.join(__dirname, "../images/nix-logo-sm.png"),
+          // The large logo is used on the login page
+          largeLogoPath: path.join(__dirname, "../images/nix-logo.png"),
+          faviconPath: path.join(__dirname, "../images/favicon.ico"),
+        }),
+        {
+          translations: {
+            es: path.join(__dirname, "translations/es.json"),
+          },
+        },
+      ],
+    }),
   };
 }
