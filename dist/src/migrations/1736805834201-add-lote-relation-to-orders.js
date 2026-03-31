e25ceca30e91c94728c1428f741ec4a8a7cf2930
Jesus Lopez <jesussflr@gmail.com>
2026-03-31 14:56:37 -0400

    feat: add lote relation to order entity via migration

--- dist/src/migrations/1736805834201-add-lote-relation-to-orders.js
+++ dist/src/migrations/1736805834201-add-lote-relation-to-orders.js
@@ -3,16 +3,31 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.AddLoteRelationToOrders1736805834201 = void 0;
 class AddLoteRelationToOrders1736805834201 {
     async up(queryRunner) {
-        await queryRunner.query(`ALTER TABLE "order" ADD "customFieldsLoteid" integer`, undefined);
-        await queryRunner.query(`ALTER TABLE "order" ADD "customFields__fix_relational_custom_fields__" boolean`, undefined);
-        await queryRunner.query(`COMMENT ON COLUMN "order"."customFields__fix_relational_custom_fields__" IS 'A work-around needed when only relational custom fields are defined on an entity'`, undefined);
-        await queryRunner.query(`ALTER TABLE "order" ADD CONSTRAINT "FK_f05e39bbd08c479f06bbb956476" FOREIGN KEY ("customFieldsLoteid") REFERENCES "lote"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`, undefined);
+        await queryRunner.query(`ALTER TABLE "order" ADD COLUMN IF NOT EXISTS "customFieldsLoteid" integer`);
+        await queryRunner.query(`ALTER TABLE "order" ADD COLUMN IF NOT EXISTS "customFields__fix_relational_custom_fields__" boolean`);
+        await queryRunner.query(`COMMENT ON COLUMN "order"."customFields__fix_relational_custom_fields__" IS 'A work-around needed when only relational custom fields are defined on an entity'`);
+        await queryRunner.query(`
+      DO $$
+      BEGIN
+        IF NOT EXISTS (
+          SELECT 1
+          FROM pg_constraint
+          WHERE conname = 'FK_f05e39bbd08c479f06bbb956476'
+        ) THEN
+          ALTER TABLE "order"
+          ADD CONSTRAINT "FK_f05e39bbd08c479f06bbb956476"
+          FOREIGN KEY ("customFieldsLoteid") REFERENCES "lote"("id")
+          ON DELETE NO ACTION ON UPDATE NO ACTION;
+        END IF;
+      END
+      $$;
+    `);
     }
     async down(queryRunner) {
-        await queryRunner.query(`ALTER TABLE "order" DROP CONSTRAINT "FK_f05e39bbd08c479f06bbb956476"`, undefined);
-        await queryRunner.query(`COMMENT ON COLUMN "order"."customFields__fix_relational_custom_fields__" IS 'A work-around needed when only relational custom fields are defined on an entity'`, undefined);
-        await queryRunner.query(`ALTER TABLE "order" DROP COLUMN "customFields__fix_relational_custom_fields__"`, undefined);
-        await queryRunner.query(`ALTER TABLE "order" DROP COLUMN "customFieldsLoteid"`, undefined);
+        await queryRunner.query(`ALTER TABLE "order" DROP CONSTRAINT IF EXISTS "FK_f05e39bbd08c479f06bbb956476"`);
+        await queryRunner.query(`COMMENT ON COLUMN "order"."customFields__fix_relational_custom_fields__" IS 'A work-around needed when only relational custom fields are defined on an entity'`);
+        await queryRunner.query(`ALTER TABLE "order" DROP COLUMN IF EXISTS "customFields__fix_relational_custom_fields__"`);
+        await queryRunner.query(`ALTER TABLE "order" DROP COLUMN IF EXISTS "customFieldsLoteid"`);
     }
 }
 exports.AddLoteRelationToOrders1736805834201 = AddLoteRelationToOrders1736805834201;
--- src/migrations/1736805834201-add-lote-relation-to-orders.ts
+++ src/migrations/1736805834201-add-lote-relation-to-orders.ts
@@ -1,19 +1,36 @@
-import {MigrationInterface, QueryRunner} from "typeorm";
+import { MigrationInterface, QueryRunner } from "typeorm";
 
 export class AddLoteRelationToOrders1736805834201 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`ALTER TABLE "order" ADD COLUMN IF NOT EXISTS "customFieldsLoteid" integer`);
+    await queryRunner.query(`ALTER TABLE "order" ADD COLUMN IF NOT EXISTS "customFields__fix_relational_custom_fields__" boolean`);
+    await queryRunner.query(
+      `COMMENT ON COLUMN "order"."customFields__fix_relational_custom_fields__" IS 'A work-around needed when only relational custom fields are defined on an entity'`
+    );
+    await queryRunner.query(`
+      DO $$
+      BEGIN
+        IF NOT EXISTS (
+          SELECT 1
+          FROM pg_constraint
+          WHERE conname = 'FK_f05e39bbd08c479f06bbb956476'
+        ) THEN
+          ALTER TABLE "order"
+          ADD CONSTRAINT "FK_f05e39bbd08c479f06bbb956476"
+          FOREIGN KEY ("customFieldsLoteid") REFERENCES "lote"("id")
+          ON DELETE NO ACTION ON UPDATE NO ACTION;
+        END IF;
+      END
+      $$;
+    `);
+  }
 
-   public async up(queryRunner: QueryRunner): Promise<any> {
-        await queryRunner.query(`ALTER TABLE "order" ADD "customFieldsLoteid" integer`, undefined);
-        await queryRunner.query(`ALTER TABLE "order" ADD "customFields__fix_relational_custom_fields__" boolean`, undefined);
-        await queryRunner.query(`COMMENT ON COLUMN "order"."customFields__fix_relational_custom_fields__" IS 'A work-around needed when only relational custom fields are defined on an entity'`, undefined);
-        await queryRunner.query(`ALTER TABLE "order" ADD CONSTRAINT "FK_f05e39bbd08c479f06bbb956476" FOREIGN KEY ("customFieldsLoteid") REFERENCES "lote"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`, undefined);
-   }
-
-   public async down(queryRunner: QueryRunner): Promise<any> {
-        await queryRunner.query(`ALTER TABLE "order" DROP CONSTRAINT "FK_f05e39bbd08c479f06bbb956476"`, undefined);
-        await queryRunner.query(`COMMENT ON COLUMN "order"."customFields__fix_relational_custom_fields__" IS 'A work-around needed when only relational custom fields are defined on an entity'`, undefined);
-        await queryRunner.query(`ALTER TABLE "order" DROP COLUMN "customFields__fix_relational_custom_fields__"`, undefined);
-        await queryRunner.query(`ALTER TABLE "order" DROP COLUMN "customFieldsLoteid"`, undefined);
-   }
-
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`ALTER TABLE "order" DROP CONSTRAINT IF EXISTS "FK_f05e39bbd08c479f06bbb956476"`);
+    await queryRunner.query(
+      `COMMENT ON COLUMN "order"."customFields__fix_relational_custom_fields__" IS 'A work-around needed when only relational custom fields are defined on an entity'`
+    );
+    await queryRunner.query(`ALTER TABLE "order" DROP COLUMN IF EXISTS "customFields__fix_relational_custom_fields__"`);
+    await queryRunner.query(`ALTER TABLE "order" DROP COLUMN IF EXISTS "customFieldsLoteid"`);
+  }
 }
